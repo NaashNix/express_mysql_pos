e51b2ec1a321391bac48ab073e7a235c3e5e297b
NaashNix <thilakshanaravindu@gmail.com>
2022-09-06 10:20:51 +0530

    item controller done

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -8,9 +8,13 @@ connection.connect((err) => {
    if (!err) {
       console.log("Connected to my sql server");
       const customerTableQuery =
-         "CREATE TABLE IF NOT EXISTS Item(code VARCHAR(255) NOT NULL," +
-         "description VARCHAR(255),qtyOnHand INT," +
-         "unitPrice double,CONSTRAINT PRIMARY KEY (code))";
+         "CREATE TABLE IF NOT EXISTS Item(" +
+         "code VARCHAR(255) NOT NULL," +
+         "description VARCHAR(255)," + 
+         "qtyOnHand INT," +
+         "unitPrice double," + 
+         "CONSTRAINT PRIMARY KEY (code))";
+
       connection.query(customerTableQuery, (err, result) => {
          if (result.warningCount === 0) {
             console.log("item table created");
@@ -26,8 +30,8 @@ connection.connect((err) => {
 const getAllItems = (req,res) => {
    const query = 'SELECT * FROM Item';
    connection.query(query, (error, result) => {
-         if (err){
-            res.status(500).send(err.sqlMessage);
+         if (error){
+            res.status(500).send(error.sqlMessage);
          }else {
             res.send(result);
          }
@@ -41,7 +45,7 @@ const saveItem = (req,res) => {
    const qtyOnHand = req.body.qtyOnHand;
    const unitPrice = req.body.unitPrice;
 
-   connection.query(query, (error, result) => {
+   connection.query(query,[code, description, qtyOnHand, unitPrice] ,(error) => {
       if (!error){
          res.send('item saved');
       }else {
@@ -52,4 +56,55 @@ const saveItem = (req,res) => {
 }
 
 
-module.exports = {getAllItems, saveItem};
+const updateItem = (req, res) => {
+   const query = "UPDATE Item SET description =?, qtyOnHand =?, unitPrice =? WHERE code =?"
+   const code = req.body.code;
+   const description = req.body.description;
+   const qtyOnHand = req.body.qtyOnHand;
+   const unitPrice = req.body.unitPrice;
+
+   connection.query(query, [description, qtyOnHand, unitPrice,code], (error,result) => {
+      if (result.affectedRows > 0) {
+         res.send('item updated');
+      } else if ( result.affectedRows == 0){
+         res.send('item not found!');
+      }else {
+         res.send(error.sqlMessage);
+      }
+   });
+
+}
+
+const deleteItem = (req,res) => {
+   const code = req.params.code;
+   const query = "DELETE FROM Item WHERE code=? ";
+   connection.query(query, code, (err, result) => {
+      if (result.affectedRows > 0) {
+         res.send('Item deleted!');
+      } else {
+         res.send('no Item found!');
+      }
+      if (err) {
+         res.status(500).send(err.sqlMessage);
+      }
+   });
+}
+
+const searchItem = (req,res) => {
+   const code = req.params.code;
+   const query = "SELECT * FROM Item WHERE code=?";
+   connection.query(query, code, (error, result) => {
+      const exist = result.length;
+      if (exist) {
+         res.send(result);
+      } else {
+         res.send('Item not found!');
+      }
+
+      if (error) {
+         res.status(500).send(error.sqlMessage);
+      }
+   });
+}
+
+module.exports = {getAllItems, saveItem, updateItem, deleteItem, searchItem};
--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -4,6 +4,8 @@ const itemController = require('../controllers/ItemController');
 
 router.post('/', itemController.saveItem);
 router.get('/',itemController.getAllItems);
-
+router.put('/', itemController.updateItem);
+router.delete('/:code',itemController.deleteItem);
+router.get('/:code', itemController.searchItem);
 
 module.exports = router;
